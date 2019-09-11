11f931b329ec49c78b730a13a685ad61be79928a
David Andersson <david.andersson12@learnet.se>
2019-09-11 10:27:46 +0200

    Lade till skelett med variabler och metoder samt getters och setters

---
+++ src/Message.java
@@ -0,0 +1,49 @@
+import java.util.Date;
+
+public class Message {
+    private Date createdAt;
+    private Date updatedAt;
+    private String message;
+    private String author;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    /*public void update(String) {
+
+    }*/
+}
